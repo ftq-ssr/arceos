6e891282a867e360c9893fd8e15843f9442e68a0
ftq <1310360890@qq.com>
2023-11-15 20:14:42 +0800

    step 1

--- scripts/make/qemu.mk
+++ scripts/make/qemu.mk
@@ -17,7 +17,8 @@ qemu_args-x86_64 := \
 qemu_args-riscv64 := \
   -machine virt \
   -bios default \
-  -kernel $(OUT_BIN)
+  -kernel $(OUT_BIN)\
+  -drive if=pflash,file=$(CURDIR)/payload/apps.bin,format=raw,unit=1
 
 qemu_args-aarch64 := \
   -cpu cortex-a72 \
@@ -28,7 +29,8 @@ qemu_args-y := -m 128M -smp $(SMP) $(qemu_args-$(ARCH))
 
 qemu_args-$(BLK) += \
   -device virtio-blk-$(vdev-suffix),drive=disk0 \
-  -drive id=disk0,if=none,format=raw,file=$(DISK_IMG)
+  -drive id=disk0,if=none,format=raw,file=$(DISK_IMG) 
+
 
 qemu_args-$(NET) += \
   -device virtio-net-$(vdev-suffix),netdev=net0
